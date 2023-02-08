f3c0788ff7059c0ce2fa03301535bc92f290a113
Pedro Henrique Mendes <peedrohenriqm@gmail.com>
2023-02-07 22:04:49 -0300

    07-02

---
+++ main.js
@@ -0,0 +1,48 @@
+const botao = document.getElementById("botoes");
+
+botao.addEventListener("click", (evento)=> {
+    const tipoBurguer = evento.target.value;
+    console.log(tipoBurguer);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function burguerCarne (evento) {
+    const hamburguer = document.getElementById("hamburguer");
+    hamburguer.innerHTML = `<div id="darthvader">
+    <p> Darth Vader </p>
+    <p> Com pão do lado sombrio da força, queijo diretamente de tatooine, carne de bisonhos raivosos e muito bacon dos deliciosos rebeldes </p>
+    </div>`;
+}  */
