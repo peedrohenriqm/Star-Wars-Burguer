e118d19b93e7593efbf6aba2e072bfab3aec29a9
Pedro Henrique Mendes <peedrohenriqm@gmail.com>
2023-02-09 22:30:50 -0300

    09-02

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
-const botao = document.getElementById("botoes");
+const cardapio = [
+    {
+        id: 0,
+        nome: "Leia  Organa",
+        categoria: "frango",
+        preco: 6,
+        descricao:"muito delicioso, pae"
+    },
+    {
+        id: 1,
+        nome: "luke",
+        categoria: "carne",
+        preco: 15,
+        descricao:"delicia, cara"
+    },
+    /* {
+        id: 2,
+        nome: "ioda",
+        categoria: "carne",
+        preco: 25,
+        descricao:"delicia, cara"
+    },
+    {
+        id: 3,
+        nome: "Vader",
+        categoria: "carne",
+        preco: 13,
+        descricao:"delicia, cara"
+    },
+    {
+        id: 4,
+        nome: "Dath ",
+        categoria: "kids",
+        preco: 40,
+        descricao:"delicia, cara"
+    } */
 
-botao.addEventListener("click", (evento)=> {
-    const tipoBurguer = evento.target.value;
-    console.log(tipoBurguer);
-})
+];
+
+const sessaoCentral = document.getElementById("sessao-central");
+const botoes = document.getElementById("grupo-botoes")
+
+const itensMenu = cardapio.map((burguer)=>{
+    exibirBurguerNaTela(cardapio);
+});
+
+function exibirBurguerNaTela(listaDeBurguer){
+    listaDeBurguer.forEach(burguer => {
+        sessaoCentral.innerHTML+= `
+            <div class="burguer">
+                <p>${burguer.nome}</p>
+                <p class="preco">${burguer.preco}</p>
+                <p class="burguer-text">${burguer.descricao}</p>
+            </div>`;
+    });
+}
 
 
 
@@ -32,17 +82,3 @@ botao.addEventListener("click", (evento)=> {
 
 
 
-
-
-
-
-
-
-
-/* function burguerCarne (evento) {
-    const hamburguer = document.getElementById("hamburguer");
-    hamburguer.innerHTML = `<div id="darthvader">
-    <p> Darth Vader </p>
-    <p> Com pão do lado sombrio da força, queijo diretamente de tatooine, carne de bisonhos raivosos e muito bacon dos deliciosos rebeldes </p>
-    </div>`;
-}  */
